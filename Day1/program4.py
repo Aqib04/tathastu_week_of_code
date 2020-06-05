b5275ea2ce45eab881ba108d8d9062e1ae4c7d05
Mohiuddin Khan <66483332+Aqib04@users.noreply.github.com>
2020-06-05 21:02:50 +0530

    Create program4.py

---
+++ Day1/program4.py
@@ -0,0 +1,7 @@
+costprice=float(input("Enter the cost price of the product:\n"))
+sellprice=float(input("Enter the sell price of the product:\n"))
+profit=sellprice-costprice
+newsellprice=1.05*profit+costprice
+print("sell profit is:-",profit)
+print("5% profit increase then the price is:-",newsellprice)
+
