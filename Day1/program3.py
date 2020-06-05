00ed97508f6f3acf37907e84b5e5dbcf4a74a913
Mohiuddin Khan <66483332+Aqib04@users.noreply.github.com>
2020-06-05 21:02:20 +0530

    Create program3.py

---
+++ Day1/program3.py
@@ -0,0 +1,11 @@
+a=int(input("Enter the value of a:-\n"))
+b=int(input("Enter the value of b:-\n"))
+a=a+b
+b=a-b
+a=a-b
+print("After Swapping")
+print("value of a:-",a)
+print("value of b:-",b)
+
+
+
